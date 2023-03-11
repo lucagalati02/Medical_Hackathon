40f53ea15b45a91b2588113ef1ba55897767f9f7
Luca Galati <lucagalati31@gmail.com>
2023-03-11 18:49:28 -0500

    Linking

--- DiagnosticAI/controllers.py
+++ DiagnosticAI/controllers.py
@@ -5,7 +5,44 @@ from DiagnosticAI import situation
 
 @app.route('/', methods=["POST", "GET"])
 def home():
-    """
-    Displays the DiagnosticAI home page
-    """
-    return render_template('home.html')
+    title = ""
+    message = ""
+    if request.method == "POST":
+        if 'refresh' in request.form:
+            return redirect(url_for('symptoms'))
+        elif 'validate' in request.form:
+            return render_template("home.html", title="", message="")
+    else:
+        title = ""
+        message = ""
+        return render_template("home.html", title=title, message=message)
+
+
+@app.route('/symptoms', methods=["POST", "GET"])
+def symptoms():
+    title = "Patient Symptoms"
+    message, correct_diagnosis = situation.situation()
+    session['message'] = message
+    session['correct_diagnosis'] = correct_diagnosis
+    if request.method == "POST":
+        if 'refresh' in request.form:
+            return redirect(url_for('symptoms'))
+        elif 'validate' in request.form:
+            session['diagnosis_attempt'] = request.form['diagnosis_attempt']
+            return redirect(url_for('results'))
+    return render_template("home.html", title=title, message=message)
+
+
+@app.route('/results', methods=["POST", "GET"])
+def results():
+    correct_diagnosis = session['correct_diagnosis']
+    diagnosis_attempt = session['diagnosis_attempt']
+    status = situation.diagnosing(correct_diagnosis, diagnosis_attempt)
+    if status == "True":
+        title = "Correct!"
+        message = ""
+        return render_template("home.html", title=title, message=message)
+    else:
+        title = "Incorrect!"
+        message = "Correct diagnosis: " + correct_diagnosis
+        return render_template("home.html", title=title, message=message)
--- DiagnosticAI/situation.py
+++ DiagnosticAI/situation.py
@@ -6,7 +6,7 @@ openai.api_key = API_KEY
 
 def situation():
     sit = openai.Completion.create(
-        engine="text-davinci-003", prompt="I playing a complex game where I do diagnostic medicine, give me a description of a patient to diagnose, and give me a short diagnosis. Format it as such... Patient Symptoms: patient symptoms listed here / Patient Diagnosis: the diagnosis of the patient is said here", max_tokens=1500)
+        engine="text-davinci-003", prompt="I playing a complex game where I do diagnostic medicine, give me a description of a patient to diagnose, and give me a short diagnosis. Format it as such... Patient Symptoms: patient symptoms listed here / Patient Diagnosis: the diagnosis of the patient is said here", max_tokens=2000)
     symptoms = ""
     diagnosis = ""
     for data in sit.choices[0].text.split("/"):
@@ -20,8 +20,8 @@ def situation():
 
 def diagnosing(diagnosis, diagnosis_attempt):
     attempt = openai.Completion.create(engine="text-davinci-003", prompt=(
-        "Can you tell me if these 2 patient diagnoses match? Answer in 'True' or 'False'.\n1. " + diagnosis + "\n2. " + diagnosis_attempt), max_tokens=2)
+        "Can you tell me if these 2 patient diagnoses are similar? Answer in 'True' or 'False'.\n1. " + diagnosis + "\n2. " + diagnosis_attempt), max_tokens=2)
     if "True" in attempt.choices[0].text:
         return "True"
     elif "False" in attempt.choices[0].text:
-        return "False..." + diagnosis_attempt
+        return "False"
